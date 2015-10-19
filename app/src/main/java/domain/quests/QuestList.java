2f5eaee9af19a61c15c6dd2704cf273b2fd61a59
gustavo95 <gustavo_costa_gomes@hotmail.com>
2015-10-19 17:19:34 -0300

    add more quests

--- app/src/main/java/domain/quests/QuestList.java
+++ app/src/main/java/domain/quests/QuestList.java
@@ -22,9 +22,18 @@ public class QuestList {
     }
 
     private void insertQuests(){
-        quests.add(new Quest("Contra a Colmeia","A Colmeia despertou. Esta é a história da nova guerra da Colmeia contra a Luz."
+
+        quests.add(new Quest("Ascensão Arcana", "Sua ascensão das cinzas de um mundo destruído."
+                ,"Experiencia, armadura, nave.", "7", R.drawable.warlock_quest));
+        quests.add(new Quest("Ascensão Caçadora", "Sua ascensão das cinzas do Colapso."
+                ,"Experiencia, armadura, nave.", "7", R.drawable.hunter_quest));
+        quests.add(new Quest("Ascensão Titã", "Sua ascensão das cinzas de um mundo destruído."
+                ,"Experiencia, armadura, nave.", "7", R.drawable.titan_quest));
+        quests.add(new Quest("Nada restou além da queda", "A história e a batalha dos Decaídos e da Cidade pelo futuro da Terra."
+                ,"Experiencia, armaduras.", "11", R.drawable.quest1));
+        quests.add(new Quest("Contra a Colmeia", "A Colmeia despertou. Esta é a história da nova guerra da Colmeia contra a Luz."
                 ,"Experiencia, armas, armaduras.", "7", R.drawable.quest2));
-        quests.add(new Quest("Fruto do Jardim","A história da estranha misteriosa e seu conto do Jardim Sombrio."
+        quests.add(new Quest("Fruto do Jardim", "A história da estranha misteriosa e seu conto do Jardim Sombrio."
                 ,"Experiencia, reputação, armas, armaduras.", "13", R.drawable.quest3));
     }
 }
