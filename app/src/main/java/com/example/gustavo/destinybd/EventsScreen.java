e6fe8469ac50018a77f4c4f81020b6e890029aff
gustavo95 <gustavo_costa_gomes@hotmail.com>
2015-10-25 15:00:19 -0300

    conclude events

--- app/src/main/java/com/example/gustavo/destinybd/EventsScreen.java
+++ app/src/main/java/com/example/gustavo/destinybd/EventsScreen.java
@@ -1,9 +1,17 @@
 package com.example.gustavo.destinybd;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -11,6 +19,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
+
+import services.TimeManagement;
 
 public class EventsScreen extends Activity {
 
@@ -20,42 +30,100 @@ public class EventsScreen extends Activity {
         setContentView(R.layout.activity_events_screen);
 
         Intent intent = getIntent();
+
+        TimeManagement tm = new TimeManagement();
+        raidCount(tm.getDiference(tm.getTime(), tm.nextDayOfWeek(Calendar.TUESDAY,9)));
+        xurCount(tm.getDiference(tm.getTime(), tm.nextDayOfWeek(Calendar.FRIDAY, 9)));
+        prisonCount(tm.getDiference(tm.getTime(), tm.nextDayOfWeek(Calendar.TUESDAY, 9)));
+        trialCount(tm.getDiference(tm.getTime(), tm.nextDayOfWeek(Calendar.FRIDAY, 17)));
     }
 
-    public void test (View view){
-        Calendar c = nextDayOfWeek(Calendar.FRIDAY);
-        Date d = c.getTime();
+    public void raidCount(long time){
 
-        DateFormat date = new SimpleDateFormat("dd-MM-yyy HH:mm:ss z");
-        date.setTimeZone(TimeZone.getTimeZone("GMT"));
+        new CountDownTimer(time, 1000) {
 
-        System.out.println(date.format(getTime()));
-        System.out.println(date.format(d));
+            public void onTick(long millisUntilFinished) {
+                DateFormat datef = new SimpleDateFormat("dd HH:mm:ss");
+                datef.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
+                Date date = new Date();
 
+                Button b = (Button) findViewById(R.id.raid);
+
+                date.setTime(millisUntilFinished);
+
+                b.setText("Incurções: " + datef.format(date));
+            }
+
+            public void onFinish() {
+                //mTextField.setText("done!");
+            }
+        }.start();
     }
 
-    public Date getTime(){
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+00:00"));
-        Date date = cal.getTime();
+    public void xurCount(long time){
 
-        return date;
+        new CountDownTimer(time, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                DateFormat datef = new SimpleDateFormat("dd HH:mm:ss");
+                datef.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
+                Date date = new Date();
+
+                Button b = (Button) findViewById(R.id.xur);
+
+                date.setTime(millisUntilFinished);
+
+                b.setText("Xur: " + datef.format(date));
+            }
+
+            public void onFinish() {
+                //mTextField.setText("done!");
+            }
+        }.start();
     }
 
-    public long getDiference(Date date){
-        return 0;
+    public void prisonCount(long time){
+
+        new CountDownTimer(time, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                DateFormat datef = new SimpleDateFormat("dd HH:mm:ss");
+                datef.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
+                Date date = new Date();
+
+                Button b = (Button) findViewById(R.id.prison);
+
+                date.setTime(millisUntilFinished);
+
+                b.setText("Prisão dos anciões:\n" + datef.format(date));
+            }
+
+            public void onFinish() {
+                //mTextField.setText("done!");
+            }
+        }.start();
     }
 
-    public static Calendar nextDayOfWeek(int dow) {
-        Calendar date = Calendar.getInstance(TimeZone.getTimeZone("GMT+00:00"));
-        int diff = dow - date.get(Calendar.DAY_OF_WEEK);
-        if (!(diff > 0)) {
-            diff += 7;
-        }
-        date.add(Calendar.DAY_OF_MONTH, diff);
-        date.set(Calendar.HOUR_OF_DAY, 9);
-        date.set(Calendar.MINUTE, 0);
-        date.set(Calendar.SECOND, 0);
-        return date;
+    public void trialCount(long time){
+
+        new CountDownTimer(time, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                DateFormat datef = new SimpleDateFormat("dd HH:mm:ss");
+                datef.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
+                Date date = new Date();
+
+                Button b = (Button) findViewById(R.id.trial);
+
+                date.setTime(millisUntilFinished);
+
+                b.setText("Desafios de Osíris: " + datef.format(date));
+            }
+
+            public void onFinish() {
+                //mTextField.setText("done!");
+            }
+        }.start();
     }
 
 }
---
+++ app/src/main/java/services/TimeManagement.java
@@ -0,0 +1,37 @@
+package services;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Gustavo on 25/10/2015
+ */
+public class TimeManagement {
+    public Date getTime(){
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+00:00"));
+        Date date = cal.getTime();
+
+        return date;
+    }
+
+    public long getDiference(Date date1, Date date2){
+        long diference = date2.getTime() - date1.getTime();
+        return diference;
+    }
+
+    public static Date nextDayOfWeek(int dow, int hour) {
+        Calendar date = Calendar.getInstance(TimeZone.getTimeZone("GMT+00:00"));
+        int diff = dow - date.get(Calendar.DAY_OF_WEEK);
+        if (!(diff > 0)) {
+            diff += 7;
+        }
+        date.add(Calendar.DAY_OF_MONTH, diff);
+        date.set(Calendar.HOUR_OF_DAY, hour);
+        date.set(Calendar.MINUTE, 0);
+        date.set(Calendar.SECOND, 0);
+
+        Date d = date.getTime();
+        return d;
+    }
+}
